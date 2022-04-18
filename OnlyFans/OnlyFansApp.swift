76d245a6679b6cb7e26f9bae5ca07c00bd9a9c05
UlianaGritchina <ugritchina@icloud.com>
2022-04-18 09:55:26 +0300

    Initial Commit

---
+++ OnlyFans/OnlyFansApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyFansApp.swift
+//  OnlyFans
+//
+//  Created by Ульяна Гритчина on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlyFansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
